9a1b66223765d72ae8cb07c0748ff5ec6dfff7f8
Ubuntu <ubuntu@gpu-vm-a16-5.maas>
2022-02-14 00:42:34 +0000

    updates with a hat tip to PH for the commit name inspo

--- get_dataloader.py
+++ get_dataloader.py
@@ -1,7 +1,15 @@
 from gpt_k import PileDataset
 from torch.utils.data import DataLoader
+from gpt_k.utils.GPT2_Dataset import GPT2Dataset
+from gpt_k.utils.indexed_dataset import make_dataset as make_indexed_dataset
+import numpy as np
 
 def get_dataloader(batch_size):
-    dataset = PileDataset()
+    indexed_dataset = make_indexed_dataset("./data/pile_test_text_document", "mmap", skip_warmup=False)
+    total_num_of_documents = indexed_dataset.sizes.shape[0]
+    print(f"{total_num_of_documents} total documents")
+    dataset = None
+    documents = np.arange(start=0, stop=total_num_of_documents, step=1, dtype=np.int32)
+    dataset = GPT2Dataset("train_test", "pile_test_text_document", documents, indexed_dataset, 7500, 100, 1)
     dataloader = DataLoader(dataset, batch_size, num_workers=0, shuffle=True)
     return dataloader
--- multi_gpu.py
+++ multi_gpu.py
@@ -2,51 +2,34 @@
 # coding: utf-8
 
 import torch.multiprocessing as mp
-from ddp_train import infer
+from infer import infer
 from argparse import Namespace
 import wandb
 
-num_batches = 90000
-batch_size = 32
-embedding_dim = 256
-num_heads = 8
-mask = True
-dropout = 0.05
-forward_expansion = 4
-depth = 8
-seq_len = 50
-vocab_len = 52015
-device = "cuda"
-seed = -1
-lr = 0.001
-final = False
-test_every = 200
-gradient_clipping = 1
-lr_warmup = 5000
-sample_length = 600
-test_size = 25
+parameters = {
+    "num_batches": 10000,
+    "batch_size": 80,
+    "embedding_dim": 512,
+    "num_heads": 8,
+    "mask": True,
+    "dropout": 0.05,
+    "forward_expansion": 2,
+    "depth": 8,
+    "seq_len": 100,
+    "vocab_len": 52015,
+    "device": "cuda",
+    "seed": -1,
+    "lr": 0.0001,
+    "final": False,
+    "test_every": 500,
+    "gradient_clipping": 1,
+    "lr_warmup": 5000,
+    "sample_length": 600,
+    "test_size": 25,
+    "use_wandb": True
+}
 
-params = Namespace(
-    num_batches=num_batches,
-    batch_size=batch_size,
-    embedding_dim=embedding_dim,
-    num_heads=num_heads,
-    mask=mask,
-    dropout=dropout,
-    forward_expansion=forward_expansion,
-    depth=depth,
-    seq_len=seq_len,
-    vocab_len=vocab_len,
-    device=device,
-    seed=seed,
-    lr=lr,
-    final=final,
-    test_every=test_every,
-    gradient_clipping=gradient_clipping,
-    lr_warmup=lr_warmup,
-    sample_length=sample_length,
-    test_size = test_size
-)
+params = Namespace(**parameters)
 
 world_size = 1
 
