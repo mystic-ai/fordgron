8d9fea750359dee5cf908c3bb21b269ac0fefac3
Ubuntu <ubuntu@cleo.maas>
2022-07-29 09:36:33 +0000

    remove weird nesting problem

--- e.py
+++ e.py
@@ -3,6 +3,7 @@ import time
 import torch
 from transformers import GPTJForCausalLM as TransformersGPTJ
 from os.path import exists
+import requests
 
 model_kwargs = {
     "depth": 12,
@@ -33,77 +34,23 @@ for file in required_files:
     if not exists(file[1]):
         download_file(file[0], file[1])
 
-model = DALLE(model_kwargs, is_reusable=True, models_root="./de-weights").to(model_kwargs["device"])
+model = DALLE(model_kwargs, is_reusable=True).to(model_kwargs["device"])
 model.encoder.load_state_dict(torch.load("./will-dalle-files/mini/encoder.pt"), strict=False)
 model.decoder.load_state_dict(torch.load("./will-dalle-files/mini/decoder.pt"), strict=False)
 model.detokenizer.load_state_dict(torch.load("./will-dalle-files/detokenizer.pt"))
 
 image = model(
-    text='a cat made from orange peel, lemon hat', 
+    text='apple', 
     seed=-1,
     grid_size=3,
 )
 
 image[5].save("test.jpg")
 
-""" input_data = "I met a traveller"
+image = model(
+    text='apple', 
+    seed=-1,
+    grid_size=3,
+)
 
-default_inference_kwargs = {
-    "streaming": False,
-    "generation_length": 400,
-    "temperature": 1.0,
-    "top_k": 0,
-    "top_p": 0,
-    "use_cache": True
-}
-
-inference_kwargs = {
-    **default_inference_kwargs,
-}
-
-assert any(
-    [isinstance(input_data, str), isinstance(input_data, list)]
-), "Input should be a string, or a list of strings"
-if isinstance(input_data, str):
-    input_data = [input_data]
-
-# streaming should not be allowed with a batch
-
-num_batches = len(input_data)
-input_pos = 0
-
-# generate completions
-generated_texts = []
-terminate_runs = 0
-if input_pos == num_batches:
-    terminate_runs = 1
-else:
-    raw_text = input_data[input_pos]
-    input_pos += 1
-
-    if raw_text == "":
-        context_tokens = [eos_token_id]
-    else:
-        context_tokens = tokenizer.encode(raw_text)
-    context_length = len(context_tokens)
-
-sentences = ["", "", "", "", ""]
-
-now = time.time()
-for (
-    generated_tokens
-) in stream_token_ids(
-    model,
-    inference_kwargs,
-    input_tokens=[context_tokens], # wrapped in an outer list to emulate batch_len 1
-    model_seq_len=2048
-):
-    try:
-        for index, token in enumerate(generated_tokens.tolist()):
-            sentences[index] = sentences[index] + tokenizer.decode(token)
-        if True:
-            print(sentences)
-    except Exception as e:
-        print(e)
-        print("stream tokens failed")
-print(time.time() - now) """
+image[5].save("test1.jpg")
--- fordgron/README.md
+++
@@ -1,5 +0,0 @@
-# fordgron
-
-PyTorch (where possible) implementations of popular models for the Pipeline Cloud collection.
-
-> "y ford gron" means "the round table" in Welsh
